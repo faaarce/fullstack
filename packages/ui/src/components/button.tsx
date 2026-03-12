c2134e4ccf1056da09e2c1269504a5379ff700fd
faaarce <risfararie@gmail.com>
2026-03-12 22:47:53 +0700

    adding prisma

--- packages/ui/src/components/button.tsx
+++ packages/ui/src/components/button.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import type React from "react";
 
 interface ButtonProps {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 
 export const Button = (props: ButtonProps) => {
-  return <button type="button" className="bg-indigo-600 text-white p-2 rounded-lg">{props.children}</button>;
+	return (
+		<button type="button" className="bg-indigo-600 text-white p-2 rounded-lg">
+			{props.children}
+		</button>
+	);
 };
